c773d77f6526af8bb4fb942ef6c12fdd3afe978a
David Pham <david.pham@medela.ca>
2018-09-12 15:18:20 -0400

    added req to server.js and coded schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("BigMac", 0);
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('McChicken', 0);
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Spicy Chicken Burger', 0);
