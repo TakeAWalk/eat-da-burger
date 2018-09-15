5d95044567382895269c915cac98faa36ec469f7
David Pham <email@davidpham.ca>
2018-09-15 12:52:39 -0400

    copied instructions over, corrected orm

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
-var mysql = require("mysql");
+var mysql = require('mysql');
 var connection;
 
 if (process.env.JAWSDB_URL) {
@@ -9,10 +9,10 @@ if (process.env.JAWSDB_URL) {
     user: 'root',
     password: 'password',
     database: 'burgers_db' */
-    host: "bbj31ma8tye2kagi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "g2abaxmkqjlma86e",
-    password: "aqwxz941p4nyyd4j",
-    database: "c84en7gk6i142y5k"
+    host: 'bbj31ma8tye2kagi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'g2abaxmkqjlma86e',
+    password: 'aqwxz941p4nyyd4j',
+    database: 'c84en7gk6i142y5k'
   });
 }
 
--- config/orm.js
+++ config/orm.js
@@ -1,21 +1,28 @@
-var connection = require("./connection.js");
+var connection = require('./connection.js');
 
 var orm = {
-  selectAll: () => {
-    console.log("orm:selectAll");
-    connection.query("SELECT * FROM burgers;", (err, data) => {
+  selectAll: cb => {
+    var queryString = 'SELECT * FROM burgers';
+    connection.query(queryString, [tableInput], (err, result) => {
       if (err) throw err;
-
-      console.log(data);
+      cb(result);
     });
   },
-
-  insertOne: burger => {
-    console.log("Insert One");
+  insertOne: (burger_name, cb) => {
+    var queryString =
+      'INSERT INTO burgers (burger_name, devoured) VALUES (?, 0)';
+    connection.query(queryString, [burger_name], (err, result) => {
+      if (err) throw err;
+      cb(result);
+    });
   },
-
-  updateOne: burger => {
-    console.log("Update One");
+  updateOne: (burger_name, devoured, cb) => {
+    var queryString =
+      'UPDATE burgers SET burger_name = ?, devoured = ?) VALUES (?, 0)';
+    connection.query(queryString, [burger_name, devoured], (err, result) => {
+      if (err) throw err;
+      cb(result);
+    });
   }
 };
 
